f13bec25c2a15a1536c0e159f9af68e64de2f447
damiensauv <damiensauvalle@gmail.com>
2015-11-25 12:54:53 +0100

    BankAccount

--- src/main/java/model/BankAccount.java
+++ src/main/java/model/BankAccount.java
@@ -1,61 +1,21 @@
 package model;
 
-
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-
 public class BankAccount
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	public int amount;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public Inhabitant inhabitant;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public BankAccount(){
+
+	public BankAccount(int initAmount) {
 		super();
+		this.amount = initAmount;
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	public void credit(int amount) {
-		// TODO implement me	
+		this.amount += amount;
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+
+
 	public void debit(int amount) {
-		// TODO implement me	
+		this.amount -= amount;
 	}
 	
 }
