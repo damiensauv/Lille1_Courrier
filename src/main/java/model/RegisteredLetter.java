92992c57781dc50d982c253bbbc2e0205604d8c7
damiensauv <damiensauvalle@gmail.com>
2015-11-25 16:56:55 +0100

    registered

--- src/main/java/model/RegisteredLetter.java
+++ src/main/java/model/RegisteredLetter.java
@@ -3,12 +3,15 @@ package model;
 
 public class RegisteredLetter extends NotUrgentLetter
 {
-	public NotUrgentLetter letter;
-
 	public RegisteredLetter(Inhabitant sender, Inhabitant receiver, NotUrgentLetter content){
 		super(sender, receiver);
+		this.content = content;
+		this.cost = content.getCost() + 15;
+	}
 
-
+	@Override
+	public NotUrgentLetter getContent(){
+		return (NotUrgentLetter) this.content;
 	}
 
 }
