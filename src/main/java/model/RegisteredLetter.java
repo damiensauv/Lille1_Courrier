42f32552492f3f1ee0c0fb47f0cd47f6d607c04b
damiensauv <damiensauvalle@gmail.com>
2015-11-25 16:48:30 +0100

    registerd

--- src/main/java/model/RegisteredLetter.java
+++ src/main/java/model/RegisteredLetter.java
@@ -1,30 +1,14 @@
 package model;
 
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-
 public class RegisteredLetter extends NotUrgentLetter
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	public NotUrgentLetter letter;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public RegisteredLetter(Inhabitant sender, Inhabitant receiver, String content){
+
+	public RegisteredLetter(Inhabitant sender, Inhabitant receiver, NotUrgentLetter content){
 		super(sender, receiver);
+
+
 	}
 
 }
